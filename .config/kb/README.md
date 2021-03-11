a929836f3bcacb45761df1bbe4a7dc1ece407959
Francesco Giannelli <fra@localhost>
2021-03-11 14:58:33 +0100

    nix files and keyboard config

---
+++ .config/kb/README.md
@@ -0,0 +1,7 @@
+# How to use
+From inside a linux console, as root, type:
+```
+loadkeys swapcapslock.map
+```
+## Recommended
+Ensure `/etc/default/keyboard` does not have `ctrl:swapcaps` option if using KDE. Changes to this file can be applied with `setupcon` from a console.
--- bin/mmbsync.fish
+++ bin/mmbsync.fish
@@ -7,5 +7,5 @@ set MailsList (find $MailDir -maxdepth 1 -name '*@*' -printf '%f\n')
 set MailsCount (string split " " "$MailsList" | wc -l)
 
 # parallel --bar --progress mbsync ::: ${MailsList}
-# echo $MailsList | xargs -n1 -P $MailsCount mbsync
-string split " " "$MailsList" | rush --propagate-exit-status=false mbsync {}
+echo $MailsList | xargs -n1 -P $MailsCount mbsync
+# string split " " "$MailsList" | rush --propagate-exit-status=false mbsync {}
