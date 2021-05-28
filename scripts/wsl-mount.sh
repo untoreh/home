0aadf5d233890c581eb366466e4c43d63aa59747
untoreh <francesco.giannelli@yahoo.it>
2021-05-28 19:20:08 +0200

    added overlay to wsl-mount script
    this fixes `invalid argument` .exe calls from linux mounted paths

--- scripts/wsl-mount.sh
+++ scripts/wsl-mount.sh
@@ -3,7 +3,10 @@
 set -e
 
 [ -z "$UID" ] && UID=$(id -u)
-findmnt /tmp >/dev/null && { echo /tmp is already mounted; exit 1; }
+findmnt /tmp >/dev/null && {
+	echo /tmp is already mounted
+	exit 1
+}
 
 sudo mkdir -p /run/upper /run/work
 #sudo mkdir -p /run/user/$UID;
@@ -12,17 +15,26 @@ sudo mkdir -p /nix
 sudo chown $UID:$UID -R /nix
 #mkdir -p /run/user/$UID/dconf
 
+# ensure mounts
+sudo mount -a
+
+sudo mkdir -p /mnt/home/.work /mnt/home/.lower /mnt/home/$USER
+
+# mount tmp with overlay
 sudo mount -t overlay overlay -o lowerdir=/tmp,upperdir=/run/upper,workdir=/run/work /tmp
+# mount home with overlay
+sudo mount -t overlay overlay -o lowerdir=/mnt/home/.lower,upperdir=/mnt/home/$USER,workdir=/mnt/home/.work ~/
 tries=0
-while [ ! -d "/home/nix" ]; do
+while [ ! -d "/mnt/home/nix" ]; do
 	sleep 1
-        tries=$((tries+1))
-	[ $tries -gt 10 ] && { 
-		echo aborting nix mount, /home/nix not found
+	tries=$((tries + 1))
+	[ $tries -gt 10 ] && {
+		echo aborting nix mount, /mnt/home/nix not found
 		lsblk
-	       	sleep 3
-		exit 1 
+		sleep 3
+		exit 1
 	}
 done
-sudo mount --bind /home/nix /nix
+sudo mount --bind /mnt/home/nix /nix
 sudo chmod 777 /tmp && touch /tmp/.mounted
+ln -srf /mnt/c/Users/$USER ~/win
