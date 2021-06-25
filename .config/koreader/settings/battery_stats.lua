fbb36551fa7723d4d129562de3af2f24b8cf426c
untoreh <francesco.giannelli@yahoo.it>
2021-06-25 09:49:59 +0200

    ebooks config

---
+++ .config/koreader/settings/battery_stats.lua
@@ -0,0 +1,27 @@
+-- we can read Lua syntax here!
+return {
+    ["awake"] = {
+        ["percentage"] = 0,
+        ["time"] = 3625,
+    },
+    ["awake_state"] = {
+        ["percentage"] = 100,
+        ["timestamp"] = 1624548239,
+    },
+    ["charging"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+    ["charging_state"] = {
+        ["percentage"] = 100,
+        ["timestamp"] = 1624548239,
+    },
+    ["discharging"] = {
+        ["percentage"] = 0,
+        ["time"] = 3625,
+    },
+    ["sleeping"] = {
+        ["percentage"] = 0,
+        ["time"] = 0,
+    },
+}
