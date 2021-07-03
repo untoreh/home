ced6e3966fefb0e418cd0667a329af584778fbb2
untoreh <francesco.giannelli@yahoo.it>
2021-07-03 11:27:34 +0200

    add nodejs and krita to packages

--- .config/nixpkgs/overlays/packages.nix
+++ .config/nixpkgs/overlays/packages.nix
@@ -13,28 +13,31 @@ let
   };
 in {
   inherit (super)
-    # shell
-    starship eternal-terminal browsh zsh git-hub git-lfs pet tmux fzf fd bat ripgrep zoxide exa 
+  # shell
+    starship eternal-terminal browsh zsh git-hub git-lfs pet tmux fzf fd bat
+    ripgrep zoxide exa
     # utils
-    wego translate-shell weechat
+    wego translate-shell weechat krita
     # files and backups
     duplicacy borgbackup rclone syncthing
     # desktop
     mako wl-clipboard
     # theme
-    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji-blob-bin noto-fonts-extra
+    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji-blob-bin
+    noto-fonts-extra
     # input-fonts
     # system (don't use fish from nix because of locales)
-    man less thefuck glibcLocales tini libqalculate dhcp bandwhich consul haproxy rsync sshpass pandoc zip
+    man less thefuck glibcLocales tini libqalculate dhcp bandwhich consul
+    haproxy rsync sshpass pandoc zip
 
     # langs
-    go-pup nixfmt golangci-lint shfmt libtidy
+    go-pup nixfmt golangci-lint shfmt libtidy nodejs
 
     # runtimes
     python3
 
     # emacs
-    # emacsPgtkGcc  # wait for mesa-d3d12
+    # emacsPgtk  # wait for mesa-d3d12
     mu isync gnupg pinentry emacs-all-the-icons-fonts neovim;
   # dependencies
   inherit (self.weechatScripts) weechat-matrix;
