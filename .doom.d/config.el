a409669436d4456c6f7ba1457ed34382e16c91b7
untoreh <francesco.giannelli@yahoo.it>
2021-07-22 07:48:08 +0200

    autoload main session

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -11,3 +11,10 @@
 (load! "langs/langs")
 
 (message "Configuration Loaded Succesfully!")
+
+(let* ((session-name "main")
+       (session-file (concat (file-name-as-directory (doom-session-file))
+           session-name)))
+  (message "restoring %s session" session-name)
+  (file-readable-p session-file)
+  (doom-load-session session-file))
