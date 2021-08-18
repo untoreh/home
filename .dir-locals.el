d31cec0177f96fef72310e823347ed3f9dd280a7
untoreh <francesco.giannelli@yahoo.it>
2021-08-18 18:46:51 +0200

    attempt to ignore projectile directories
    by changing the find command directly as a dir-local variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((ranger-mode . ((ranger-show-hidden . t))))
+((projectile-mode . ((projectile-generic-command . "fd . -0 -H --color=never --type file --type symlink --follow --exclude .git --exclude win --exclude tmp --exclude dev --exclude mnt --exclude .venv --exclude .ipfs --exclude '.nix*'")))
+ (ranger-mode . ((ranger-show-hidden . t))))
