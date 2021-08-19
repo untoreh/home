09a115902cbe750ad71573c7aec13b0837bf72b0
untoreh <francesco.giannelli@yahoo.it>
2021-08-19 14:59:58 +0200

    add debugging functions for emacs GC

--- .doom.d/functions.el
+++ .doom.d/functions.el
@@ -41,18 +41,48 @@
 ;;              (list (cdr l) (nconc cc `(,(car l)))))
 ;;     (nconc cc `(,(car l)) `(,(cdr l)))))
 
-(comp-defun
- my/lists-longer-than (n)
- " List top level list items longer than N . Doesn't do any recursion or unwrapping except
-for dotted pair lists. "
- (let ((c 0)
-       (l (append obarray '()))
-       (long '()))
-   (dolist (o l)
-     (when (and (not (numberp o)) (boundp o))
-       (let ((val (symbol-value o)))
-         (when (and (listp val)
-                    (> (length (my/collapse-dotted-list val)) n))
-           (nconc long '(o))
-           (cl-incf c)))))
-   (list c long)))
+;; (comp-defun
+;;  my/lists-longer-than (n)
+;;  " List top level list items longer than N . Doesn't do any recursion or unwrapping except
+;; for dotted pair lists. "
+;;  (let ((c 0)
+;;        (l (append obarray '()))
+;;        (long '()))
+;;    (dolist (o l)
+;;      (when (and (not (numberp o)) (boundp o))
+;;        (let ((val (symbol-value o)))
+;;          (when (and (listp val)
+;;                     (> (length (my/collapse-dotted-list val)) n))
+;;            (nconc long '(o))
+;;            (cl-incf c)))))
+;;    (list c long)))
+
+;; (cl-defun my/benchmark-gc
+;;     (&optional (garbage-amount 16000000)
+;;                (tcount 10))
+;;   " Run garbage collection TCOUNT times after generating GARBAGE-AMOUNT bytes of garbage"
+;;   (let* ((gc-cons-threshold most-positive-fixnum)
+;;          (gc (lambda () (gcmh-time (garbage-collect)))))
+;;     ;; make sure no garbage is present,
+;;     ;; but does not guarantee all garbage is collected
+;;     (funcall gc)
+;;     (let* ((base (funcall gc))
+;;            (times '())
+;;            (ratios '())) ;; baseline without garbage
+;;       (dotimes (_ tcount)
+;;         (make-vector garbage-amount nil) ;; make garbage
+;;         (let ((tm (funcall gc))) ;; store gc time
+;;           (push tm times)
+;;           (push (- (/ tm base) 1) ratios)))
+;;       (let* ((avg-ratio (/ (-sum ratios) tcount))
+;;              (avg-time (/ (-sum times) tcount))
+;;              (pct-ratio (* 100 avg-ratio))
+;;              (ms (* 1000 (- avg-time base)))
+;;              (base-ms (* 1000 base)))
+;;         (message "Collecting %s of garbage takes %.2f%% more time (%.2f ms) than no garbage (%.2f ms)"
+;;                  (file-size-human-readable garbage-amount 'si) pct-ratio ms base-ms)))))
+;; (cl-defun my/gc-unused ()
+;;   " total unused bytes emacs lisp objects stored "
+;;   (let* ((gc (garbage-collect))
+;;          (unused (mapcar (lambda (x) (* (or (nth 1 x) 0) (or (nth 3 x) 0))) gc)))
+;;     (file-size-human-readable (reduce (lambda (x y) (+ x y)) unused) 'si)))
