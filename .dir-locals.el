e9bad7b59467c0ae356c0c2a911dafb025b88a32
untoreh <francesco.giannelli@yahoo.it>
2021-08-22 18:29:29 +0200

    minor

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((projectile-mode . ((projectile-generic-command . "fd . -0 -H --color=never --type file --type symlink --follow --exclude .git --exclude win --exclude tmp --exclude dev --exclude mnt --exclude .venv --exclude .ipfs --exclude '.nix*'")))
+((nil . ((projectile-generic-command . "fd . -0 -H --color=never --type file --type symlink --follow --exclude .git --exclude win --exclude tmp --exclude mnt --exclude .venv --exclude .ipfs --exclude '.nix*'")))
  (ranger-mode . ((ranger-show-hidden . t))))
