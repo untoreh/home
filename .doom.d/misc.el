0a60a989201ca7a0e74dba4b12c2db6e465baafe
untoreh <francesco.giannelli@yahoo.it>
2021-09-04 17:44:58 +0200

    fix WSL config
    - fix wrong conditional block
    - bind the alt-leader key to `F13`

--- .doom.d/misc.el
+++ .doom.d/misc.el
@@ -31,37 +31,39 @@
 ;; this makes sense only if we check on each frame creation
 ;; (if (and (display-graphic-p) (equal (pgtk-backend-display-class) "GdkWaylandDisplay"))
 ;; just check for env vars and run once per server start
-(if (getenv "WSLENV")
-    ;; shouldn't be needed since wslg 1.0.0.26
-    (if (and nil (getenv "WAYLAND_DISPLAY"))
-        (progn
-          (setq wl-copy-process nil)
-          (comp-defun wl-copy (text)
-                      (setq wl-copy-process (make-process :name "wl-copy"
-                                                          :buffer nil
-                                                          ;; :command '("wl-copy" "-f" "-n")
-                                                          :command '("wex" "clip.exe")
-                                                          :connection-type 'pipe))
-                      (process-send-string wl-copy-process text)
-                      (process-send-eof wl-copy-process))
-          (comp-defun wl-paste ()
-                      ;; (if (and wl-copy-process (process-live-p wl-copy-process))
+(when (getenv "WSLENV")
+  ;; shouldn't be needed since wslg 1.0.0.26
+  (if (and nil (getenv "WAYLAND_DISPLAY"))
+      (progn
+        (setq wl-copy-process nil)
+        (comp-defun wl-copy (text)
+                    (setq wl-copy-process (make-process :name "wl-copy"
+                                                        :buffer nil
+                                                        ;; :command '("wl-copy" "-f" "-n")
+                                                        :command '("wex" "clip.exe")
+                                                        :connection-type 'pipe))
+                    (process-send-string wl-copy-process text)
+                    (process-send-eof wl-copy-process))
+        (comp-defun wl-paste ()
+                    ;; (if (and wl-copy-process (process-live-p wl-copy-process))
                                         ; should return nil if we're the current paste owner
-                      (with-temp-buffer
-                        (call-process "~/bin/wex" nil t nil "pbpaste.exe")
-                        (replace-regexp-in-string "\r$" ""
-                                                  (buffer-substring-no-properties (point-min) (point-max))))
-                      ;; )
-                      )
-          (setq interprogram-cut-function #'wl-copy)
-          (setq interprogram-paste-function #'wl-paste)))
+                    (with-temp-buffer
+                      (call-process "~/bin/wex" nil t nil "pbpaste.exe")
+                      (replace-regexp-in-string "\r$" ""
+                                                (buffer-substring-no-properties (point-min) (point-max))))
+                    ;; )
+                    )
+        (setq interprogram-cut-function #'wl-copy)
+        (setq interprogram-paste-function #'wl-paste)))
   ;; WSL, windows grabs M-SPC so we rebind it to F13 (from windows side)
   ;; and use that as alt leader key
-  (setq doom-leader-alt-key "<F13>"
-        doom-localleader-alt-key "<F13> m"
-        ;; use custom temporary directory with WSL since there are permission problems with /tmp
-        ;; NOTE: ensure trailing slash /
-        temporary-file-directory "/run/upper/"))
+  (setq doom-leader-alt-key  "<269025153>"
+        doom-localleader-alt-key "<269025153> m")
+  ;; (map! doom-leader-alt-key #'doom-leader)
+  ;; (define-localleader-key!)
+  ;; use custom temporary directory with WSL since there are permission problems with /tmp
+  ;; NOTE: ensure trailing slash /
+  (setq temporary-file-directory "/run/upper/"))
 
 ;; use wslview as program (TODO: check wslu utils is installed in wsl doom PR)
 (setq browse-url-generic-program (cond ((executable-find "wslview"))
@@ -70,13 +72,9 @@
   :commands (weechat-connected-p
              weechat-connect))
 
-;; use xclip mode with WSL and X11
-;; (if (and (getenv "WSLENV") (getenv "DISPLAY") (equal (getenv "GDK_BACKEND") "x11"))
-;;     (xclip-mode t))
-;; ... actually don't if you want to paste from an emacs buffer to windows
-
 ;; not prompt for vterm compilation
-(setq vterm-always-compile-module t)
+(when (featurep! :term vterm)
+  (setq vterm-always-compile-module t))
 
 ;; save magit buffers
 ;; this doesn't work because problems with lexical scope
@@ -92,6 +90,9 @@
 
 ;; gargbace collector mode
 (use-package! gcbal
+  :init
+  (defun gcmh-mode (&rest args))
+  (defun gcmh-set-high-threshold (&rest args))
   :config
   (native-compile-async (locate-library "gcbal") t t)
   (setq
