a4841a198b5dd60eb3fa9d4e358cf83d872b86f9
untoreh <francesco.giannelli@yahoo.it>
2021-09-04 17:55:49 +0200

    custom transient

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(bazel memory-usage powershell python-x))
+ '(mini-frame-show-parameters '((bottom . 10) (width . 0.7) (left . 0.5)))
+ '(package-selected-packages
+   '(company-prescient company-box company-posframe mu4e-views xwidgets-reuse gif-screencast mini-frame bazel memory-usage powershell python-x))
  '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
