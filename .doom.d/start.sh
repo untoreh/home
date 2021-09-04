0ff601885d5bd417237064d86fe5ec4eab798f92
untoreh <francesco.giannelli@yahoo.it>
2021-09-04 17:47:26 +0200

    tweaks to emacs shell scripts
    - add teco bin wrapper
    - switch to aur pkgbuild emacs

--- .doom.d/start.sh
+++ .doom.d/start.sh
@@ -5,24 +5,21 @@ cd ~/.emacs.d
 
 # export EMACS=$(which emacs)
 # export EMACS=~/.nix-profile/bin/emacs
-# export EMACS=/usr/bin/emacs
-export EMACS="nixGL emacs"
-
-# clean eln files
-# find /run/upper/*.eln | xargs rm
+export EMACS=/usr/bin/emacs
+# export EMACS="nixGL emacs"
 
 # bin/doom clean
-bin/doom env
-bin/doom sync -ecp
+if [ -z "$1" ]; then
+	bin/doom env
+	bin/doom sync -ecp
+	export XDG_DATA_DIRS="$HOME/.nix-profile/share:$HOME/.local/share:/usr/share"
+fi
 
 # sed -r '/(WAYLAND_DISPLAY)|(GDK_BACKEND)|(CLUTTER_BACKEND)/d' -i .local/env
 
 # unset WAYLAND_DISPLAY GDK_BACKEND CLUTTER_BACKEND
 # export GDK_BACKEND=x11 CLUTTER_BACKEND=x11
-export XDG_DATA_DIRS="$HOME/.nix-profile/share:$HOME/.local/share:/usr/share"
 export GDK_BACKEND=wayland CLUTTER_BACKEND=wayland
 
 exec bin/doom run
 #exec bin/doom run --fg-daemon=server
-#exec bin/doom run
-# exec emacs --fg-daemon=server
--- .doom.d/update.sh
+++ .doom.d/update.sh
@@ -1,35 +1,34 @@
 #!/bin/sh
 set -e
 
+supc stop emacs
 cd ~/.emacs.d
 
 function clean_env {
-sed -r '/(TERM|TMUX|KITTY|COLOR|X|QT|KDE|XDG|DISPLAY|SCREEN|WINDOW|AUTO|REPO|SSH|SESSION|PID|_ID|ID_|STARSHIP|WSL).*/d' -i .local/env
+sed -r '/(TERM|TMUX|KITTY|COLOR|X|QT|KDE|XDG|DISPLAY|SCREEN|WINDOW|AUTO|REPO|SSH|SESSION|PID|_ID|ID_|STARSHIP|WSL_).*/d' -i .local/env
 }
 
 if [ "$1" == "-e" ]; then
     clean_env
     exit $?
 fi
-exit
-bin/doom purge
-bin/doom upgrade -f
 
-find . -name \*.elc | xargs rm &
->/dev/null
-find .local/ -name \*.elc | xargs rm &
->/dev/null
-find .local/ -name \*.eln | xargs rm &
->/dev/null
-find .local/ -name \*.eln | xargs rm &
->/dev/null
+bin/doom purge
+# upgrade should execute `build
+bin/doom upgrade -f
 
 bin/doom sync
 clean_env
 # ensure unicode-fonts cache is removed
 rm -f .local/cache/pcache/unicode-fonts
 
-bin/doom build
 bin/doom compile
-bin/doom compile -c
-bin/doom compile -r
+
+# fix vterm
+rm -rf ~/.emacs.d/.local/straight/build*/vterm/build
+mkdir ~/.emacs.d/.local/straight/build*/vterm/build
+cd ~/.emacs.d/.local/straight/build*/vterm/build
+cmake -DUSE_SYSTEM_LIBVTERM=no ..
+make
+
+supc start emacs
