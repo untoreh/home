e5ad334f2b04d7fb7615a348b2853054f7d046b3
untoreh <francesco.giannelli@yahoo.it>
2021-09-19 17:34:51 +0200

    fixes

--- .doom.d/functions.el
+++ .doom.d/functions.el
@@ -3,7 +3,7 @@
 
 (defun my/script-dir (&optional obj)
   " Returns the path of the function OBJ or the path where my/script-dir is defined "
-  (if load-file-name
+  (if (and load-file-name (null obj))
       (file-name-directory load-file-name)
     (f-dirname (cdr (find-function-library (or obj #'my/script-dir))))))
 
