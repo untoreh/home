899ee7f50fec90ede8a0f1c1287e35cdd05cc498
untoreh <francesco.giannelli@yahoo.it>
2021-10-30 14:34:20 +0200

    dont start franklin automatically
    also add a setup function that configures franklin without running the
    full pass

--- .doom.d/langs/franklin.jl
+++ .doom.d/langs/franklin.jl
@@ -1,5 +1,6 @@
 using Revise
 using Conda
+using Pkg: project
 
 # These must be set before initializing Franklin
 py_v = chomp(String(read(`$(joinpath(Conda.BINDIR, "python")) -c "import sys; print(str(sys.version_info.major) + '.' + str(sys.version_info.minor))"`)))
@@ -7,10 +8,20 @@ ENV["PYTHON3"] = joinpath(Conda.BINDIR, "python")
 ENV["PIP3"] = joinpath(Conda.BINDIR, "pip")
 ENV["PYTHONPATH"] = "$(Conda.LIBDIR)/python$(py_v)"
 
-using Franklin;
-const fr = Franklin;
+using Franklin; const fr = Franklin;
 using Franklin: convert_md, convert_html, pagevar, path, globvar;
+
+function setup_franklin()
+    fr.FOLDER_PATH[] = dirname(@__FILE__)
+    if isempty(fr.FOLDER_PATH[]) fr.FOLDER_PATH[] = dirname(project().path) end
+    fr.set_paths!()
+    fr.def_GLOBAL_VARS!()
+    fr.process_config()
+    # fr.fd_setup()
+end
+
+setup_franklin()
 
 include("utils.jl")
 
-frank_task = @task serve(prerender=true); schedule(frank_task)
+# frank_task = @task serve(prerender=true); schedule(frank_task)
