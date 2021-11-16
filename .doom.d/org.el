54228a2dda6d3ea33f0eff371ba2d52de2febb54
untoreh <francesco.giannelli@yahoo.it>
2021-11-16 12:07:11 +0100

    fix org faces since the size is applied
    recursively

--- .doom.d/org.el
+++ .doom.d/org.el
@@ -144,12 +144,12 @@
 ;; Theming, all from tecosaur
 (add-hook 'org-mode-hook #'+org-pretty-mode)
 (custom-set-faces!
-  '(outline-1 :weight extra-bold :height 1.25)
-  '(outline-2 :weight bold :height 1.15)
-  '(outline-3 :weight bold :height 1.12)
-  '(outline-4 :weight semi-bold :height 1.09)
-  '(outline-5 :weight semi-bold :height 1.06)
-  '(outline-6 :weight semi-bold :height 1.03)
+  '(outline-1 :weight extra-bold :height 1.5)
+  '(outline-2 :weight bold :height 0.93)
+  '(outline-3 :weight bold :height 0.93)
+  '(outline-4 :weight semi-bold :height 0.93)
+  '(outline-5 :weight semi-bold :height 0.93)
+  '(outline-6 :weight semi-bold :height 0.93)
   '(outline-8 :weight semi-bold)
   '(outline-9 :weight semi-bold))
 (custom-set-faces!
