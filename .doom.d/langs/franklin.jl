ebb61a93d5c0649f54d843c955d835fd1e248837
untoreh <francesco.giannelli@yahoo.it>
2021-12-25 14:22:06 +0100

    add pwd to python path

--- .doom.d/langs/franklin.jl
+++ .doom.d/langs/franklin.jl
@@ -6,7 +6,7 @@ using Pkg: project
 py_v = chomp(String(read(`$(joinpath(Conda.BINDIR, "python")) -c "import sys; print(str(sys.version_info.major) + '.' + str(sys.version_info.minor))"`)))
 ENV["PYTHON3"] = joinpath(Conda.BINDIR, "python")
 ENV["PIP3"] = joinpath(Conda.BINDIR, "pip")
-ENV["PYTHONPATH"] = "$(Conda.LIBDIR)/python$(py_v)"
+ENV["PYTHONPATH"] = ".:$(Conda.LIBDIR)/python$(py_v)"
 
 using Franklin; const fr = Franklin;
 using Franklin: convert_md, convert_html, pagevar, path, globvar;
