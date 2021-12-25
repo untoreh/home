24ee0882395ce1087d9eda373a5b5e792b031194
untoreh <francesco.giannelli@yahoo.it>
2021-12-25 14:23:12 +0100

    t

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,25 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mini-frame-show-parameters '((bottom . 10) (width . 0.7) (left . 0.5)))
- '(package-selected-packages
-   '(systemd csharp-mode company-prescient company-box company-posframe mu4e-views xwidgets-reuse gif-screencast mini-frame bazel memory-usage powershell python-x))
- '(safe-local-variable-values
-   '((ranger-show-hidden . t)
-     (git-commit-major-mode . git-commit-elisp-text-mode))))
+ '(ignored-local-variable-values '((magit-large-repo-set-p . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:family "Input Mono Condensed" :height 1.0))))
- '(mode-line-inactive ((t (:family "Input Mono Condensed" :height 1.0))))
  '(org-document-title ((t (:height 1.2))))
- '(outline-1 ((t (:weight extra-bold :height 1.25))))
- '(outline-2 ((t (:weight bold :height 1.15))))
- '(outline-3 ((t (:weight bold :height 1.12))))
- '(outline-4 ((t (:weight semi-bold :height 1.09))))
- '(outline-5 ((t (:weight semi-bold :height 1.06))))
- '(outline-6 ((t (:weight semi-bold :height 1.03))))
+ '(outline-1 ((t (:weight extra-bold :height 1.5))))
+ '(outline-2 ((t (:weight bold :height 0.93))))
+ '(outline-3 ((t (:weight bold :height 0.93))))
+ '(outline-4 ((t (:weight semi-bold :height 0.93))))
+ '(outline-5 ((t (:weight semi-bold :height 0.93))))
+ '(outline-6 ((t (:weight semi-bold :height 0.93))))
  '(outline-8 ((t (:weight semi-bold))))
  '(outline-9 ((t (:weight semi-bold)))))
