76ad7fb4c69d536545fb44a99e9577a97c1cb7ae
untoreh <francesco.giannelli@yahoo.it>
2021-12-29 16:42:35 +0100

    better wsl integration
    - fix large repo var

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((magit-large-repo-set-p . t)
+         (magit-commit-show-diff . nil)
+         (magit-large-repo-p . nil)
+         (magit-refresh-buffers . nil)
          (projectile-generic-command . "fd . -0 -H --color=never --type file --type symlink --follow --exclude .git --exclude win --exclude tmp --exclude mnt --exclude .venv --exclude .ipfs --exclude .nix-profile --exclude .nix-defexpr")))
  (ranger-mode . ((ranger-show-hidden . t))))
