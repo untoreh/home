01954bb634657132bc04c50eca422fad17c4e50b
untoreh <francesco.giannelli@yahoo.it>
2022-01-14 17:53:52 +0100

    julia snippets and misc

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,12 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ignored-local-variable-values '((magit-large-repo-set-p . t))))
+ '(ignored-local-variable-values '((magit-large-repo-set-p . t)))
+ '(safe-local-variable-values '((ranger-show-hidden . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:family "Input Mono Condensed" :height 0.99))))
+ '(mode-line-inactive ((t (:family "Input Mono Condensed" :height 0.99))))
  '(org-document-title ((t (:height 1.2))))
  '(outline-1 ((t (:weight extra-bold :height 1.5))))
  '(outline-2 ((t (:weight bold :height 0.93))))
