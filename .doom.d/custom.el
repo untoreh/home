f8de29c87844337cff59b45a640434441959275c
untoreh <francesco.giannelli@yahoo.it>
2022-01-23 10:15:38 +0100

    t

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((magit-large-repo-set-p . t)))
- '(safe-local-variable-values '((ranger-show-hidden . t))))
+ '(safe-local-variable-values
+   '((julia-repl-enable-snoop)
+     (julia-repl-enable-revise)
+     (ranger-show-hidden . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
