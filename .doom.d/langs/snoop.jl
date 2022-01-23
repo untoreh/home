e518aebd2ba7524f356fe6ce5911b41e5b81b84e
untoreh <francesco.giannelli@yahoo.it>
2022-01-23 10:15:21 +0100

    snoop pkg function

--- .doom.d/langs/snoop.jl
+++ .doom.d/langs/snoop.jl
@@ -1,20 +1,38 @@
 using Pkg
 let proj = Pkg.project()
+    projpath = dirname(proj.path)
+    testpath = joinpath(projpath, "test")
     if "SnoopCompile" ∈ keys(proj.dependencies)
-        @eval using SnoopCompile
+        @eval using SnoopCompile; using ProfileView
     else
         @eval using TOML
-        projpath = dirname(proj.path)
-        testpath = joinpath(projpath, "test")
         testcfg = joinpath(testpath, "Project.toml")
         if !ispath(testcfg) || "SnoopCompile" ∉ keys(TOML.parsefile(testcfg)["deps"])
             mkpath(testpath)
             Pkg.activate(testpath)
             Pkg.add("SnoopCompile")
+            Pkg.add("ProfileView")
             Pkg.activate(projpath)
         end
         push!(LOAD_PATH, testpath)
-        @eval using SnoopCompile
+        @eval using SnoopCompile; using ProfileView
     end
-    eval(Meta.parse("using $(proj.name)"))
+    arg_proj = "--project=$projpath"
+    scriptpath = joinpath(projpath, "deps", "SnoopCompile", "script.jl")
+    # eval(Meta.parse("using $(proj.name)"))
+    @eval function pkgsnoop()
+        @info "Snooping..."
+        logfile = "snoop.log"
+        precdir = "deps/precompiles"
+        rm(precdir; force=true, recursive=true)
+        SnoopCompile.@snoopc [$arg_proj] logfile begin
+            ENV["SNOOP_COMPILER"] = "1"
+            include($scriptpath)
+            # eval(Meta.parse("using $($proj.name)"))
+        end
+        data = SnoopCompile.read(logfile)
+        pc = SnoopCompile.parcel(reverse!(data[2]))
+        SnoopCompile.write(precdir, pc)
+        @info "Snooped!"
+    end
 end
