03ab5b8f4ca3195654c016da7ae183d301aaad5b
untoreh <francesco.giannelli@yahoo.it>
2022-01-25 14:53:41 +0100

    btcli manual rsync

--- .config/fish/trades.fish
+++ .config/fish/trades.fish
@@ -8,3 +8,8 @@ end
 set FQT_DIR ~/.tmp/freqtrade
 alias fqtt "$FQT_DIR/scripts/exec.sh bash -li"
 alias qclean "dkr run -v ~/.tmp/lean_user:/lean_user --net host -it --name lean -w /Lean --entrypoint '/bin/bash' quantconnect/lean -li"
+
+function btrsmp
+    ssh mbx jle ~/scripts/crypto/btcli.jl resample $argv 2>/dev/null
+    test $status -eq 0 && ssh mbx /usr/bin/rsync -r ~/.cache/Backtest.jl/data home:~/.cache/Backtest.jl/data 2>/dev/null
+end
