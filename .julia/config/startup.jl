c3c678a33ff68c714a5eb99452ce4254d3b43797
untoreh <francesco.giannelli@yahoo.it>
2022-02-03 16:51:02 +0100

    julia delete history function

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -13,3 +13,33 @@ end
 end
 loadpy() = include("$(ENV["HOME"])/.julia/config/python.jl")
 
+function debug!()
+    if in("JULIA_DEBUG", keys(ENV))
+        delete!(ENV, "JULIA_DEBUG")
+    else
+        ENV["JULIA_DEBUG"] = "all"
+    end
+end
+
+@doc "Remove the last `n` commands from the julia repl history file."
+function deletehistory!(n = 1)
+    @eval using REPL
+    for _ in 1:n pop!(Base.active_repl.mistate.current_mode.hist.history) end
+    open(REPL.find_hist_file(), "r+") do s
+        pos = position(seekend(s))
+        while n > 0
+            seek(s, pos-8)
+            c = read(s, 8)
+            # counting  commands
+	        if c == b"# time: "
+                seek(s, pos-9)
+                nl = read(s, 1)
+                nl == b"\n" && (n -= 1)
+            end
+            pos -= 1
+        end
+        seek(s, 0)
+        truncate(s, pos-8)
+    end
+    nothing
+end
