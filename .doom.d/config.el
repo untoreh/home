d5fce29f89aeee23bb565d8d6413a81ddd366fe4
untoreh <francesco.giannelli@yahoo.it>
2022-03-21 16:50:30 +0100

    refactor: make config loading more verbose

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,20 +1,21 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(load! "vars")
-(load! "functions")
-(load! "misc")
-(load! "bindings")
-(load! "theme")
-(load! "text")
-(load! "org")
-(load! "mail")
-(load! "completion")
-(load! "langs/langs")
-(load! "projects")
-(after! magit
-  (load! "git"))
-;; (load! "maths.el")
-;; (load! "debug")
-
+(let ((force-load-messages t))
+  (load! "vars")
+  (load! "functions")
+  (load! "misc")
+  (load! "bindings")
+  (load! "theme")
+  (load! "text")
+  (load! "org")
+  (load! "mail")
+  (load! "completion")
+  (load! "langs/langs")
+  (load! "projects")
+  (after! magit
+    (load! "git"))
+  ;; (load! "maths.el")
+  ;; (load! "debug")
+  )
 
 (message "Configuration Loaded Successfully!")
