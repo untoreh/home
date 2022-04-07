c70c23439e3136b5c7362e4a1352b99d875f4381
untoreh <francesco.giannelli@yahoo.it>
2022-04-07 14:51:08 +0000

    feat: tree-sitter config on alpine

--- .doom.d/langs/langs.el
+++ .doom.d/langs/langs.el
@@ -2,24 +2,8 @@
 
 (setq enabled-langs '(python rust julia racket raku json markdown org emacs-lisp))
 
-(use-package! tree-sitter
-  :when (bound-and-true-p module-file-suffix)
-  :hook (prog-mode . tree-sitter-mode)
-  :hook (tree-sitter-after-on . tree-sitter-hl-mode)
-  :config
-  (require 'tree-sitter-langs)
-  (defadvice! doom-tree-sitter-fail-gracefully-a (orig-fn &rest args)
-    "Don't break with errors when current major mode lacks tree-sitter support."
-    :around #'tree-sitter-mode
-    (condition-case e
-	(apply orig-fn args)
-      (error
-       (unless (string-match-p (concat "^Cannot find shared library\\|"
-				       "^No language registered\\|"
-				       "cannot open shared object file")
-			       (error-message-string e))
-	 (signal (car e) (cadr e)))))))
 
+(load! "tree-sitter")
 (load! "lisp")
 (load! "spell")
 (if (featurep! :lang shell)
---
+++ .doom.d/langs/tree-sitter.el
@@ -0,0 +1,30 @@
+;;; langs/tree-sitter.el -*- lexical-binding: t; -*-
+
+(use-package! tree-sitter
+  :when (and nil bound-and-true-p module-file-suffix)
+  :hook (prog-mode . tree-sitter-mode)
+  :hook (tree-sitter-after-on . tree-sitter-hl-mode)
+  :init
+  (setq tree-sitter-langs-grammar-dir "/usr/lib/tree-sitter"
+        tsc-dyn-get-from '(:compilation))
+  (let ((rflags (getenv "RUSTFLAGS")))
+    (defadvice! tree-sitter-set-flags nil :before #'tsc-dyn-get--build
+      (setenv "RUSTFLAGS" "-C target-feature=-crt-static"))
+    (defadvice! tree-sitter-unset-flags nil :after #'tsc-dyn-get--build
+      (setenv "RUSTFLAGS" rflags))
+    (defadvice! tree-sitter-bin-dir nil :override #'tree-sitter-langs--bin-dir
+      tree-sitter-langs-grammar-dir)
+    (advice-remove 'tree-sitter-bin-dir #'tree-sitter-langs--bin-dir))
+  :config
+  (require 'tree-sitter-langs)
+  (defadvice! doom-tree-sitter-fail-gracefully-a (orig-fn &rest args)
+    "Don't break with errors when current major mode lacks tree-sitter support."
+    :around #'tree-sitter-mode
+    (condition-case e
+	(apply orig-fn args)
+      (error
+       (unless (string-match-p (concat "^Cannot find shared library\\|"
+				       "^No language registered\\|"
+				       "cannot open shared object file")
+			       (error-message-string e))
+	 (signal (car e) (cadr e)))))))
