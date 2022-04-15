3910ac56ff2e2714138778c89e5281a3c0016aa6
untoreh <francesco.giannelli@yahoo.it>
2022-04-15 11:18:22 +0200

    feat: discord script cmd

--- .config/systemd/scripts/check_mount.sh
+++ .config/systemd/scripts/check_mount.sh
@@ -8,17 +8,23 @@ function check_mount() {
     [ ! -e $mpath ] && mkdir -p $mpath
     if mountpoint -q $mpath; then
         touch ${mpath}/.alive || {
-        sudo umount -l $mpath
-        sudo mount UUID=$part_uuid $mpath
+        timeout 10 $mpath || sudo umount -l $mpath
+        timeout 10 sudo mount UUID=$part_uuid $mpath
+	if [ $? -neq 0 ]; then
+		discord-notify "Mediabox: can't mount storage to $mpath !"
+	fi
         }
     else
         sudo mount UUID=$part_uuid $mpath
+	if [ $? -neq 0 ]; then
+		discord-notify "Mediabox: can't mount storage to $mpath !"
+	fi
     fi
 }
 
 while true; do
     check_mount 77c665cc-a3e3-4fc7-8cdb-5192cfdffc3c ~/mnt/data
     check_mount 61c9a1cc-c96a-4f94-af3f-864f83167b5d ~/volatile/chains
-    check_mount b112125a-bd41-4892-8f64-1a738d006b31 ~/volatile/backups
+    check_mount 8116c5d9-ea77-451d-8774-5585b130460e ~/volatile/backups
     sleep 360
 done
