3f3fb3fd9b65e42a2114f01948b07ea99f2c35b1
untoreh <francesco.giannelli@yahoo.it>
2022-08-13 11:58:19 +0200

    chore(nix): remove python packages

--- .config/nixpkgs/overlays/userPackages/packages.nix
+++ .config/nixpkgs/overlays/userPackages/packages.nix
@@ -9,12 +9,12 @@ let
     #overlays = [ ]; # no overlays inside overlay (infinite recursion)
   };
 in {
-  inherit (super)
+  inherit (unstable)
   # shell
     eternal-terminal browsh zsh git-hub git-lfs delta pet tmux fzf fd
     ripgrep zoxide exa nushell starship direnv bat pastel
     # utils
-    wego translate-shell weechat krita calibre koreader youtube-dl djvulibre unoconv wordnet
+    wego translate-shell weechat krita calibre koreader youtube-dl djvulibre unoconv wordnet aspell
     # files and backups
     duplicacy borgbackup rclone syncthing inotify-tools
     # desktop
@@ -26,13 +26,13 @@ in {
     noto-fonts-extra sweet emojione fantasque-sans-mono julia-mono fira fira-code fira-code-symbols hack-font ia-writer-duospace
     powerline-fonts material-design-icons barlow
     # system (don't use fish from nix because of locales)
-    man less thefuck glibcLocales tini libqalculate dhcp bandwhich consul
-    haproxy rsync sshpass pandoc zip docker cachix msmtp catatonit
+    man less thefuck glibcLocales tini libqalculate dhcp bandwhich consul lld
+    haproxy rsync sshpass pandoc zip cachix msmtp catatonit
 
     # langs
-    go-pup nixfmt golangci-lint shfmt libtidy nodejs shellcheck jq enchant2 ispell languagetool
+    pup nixfmt golangci-lint shfmt html-tidy nodejs shellcheck jq enchant2 ispell languagetool
     chrpath pipenv jre;
-  inherit (unstable) mu isync goimapnotify gnupg keychain pinentry emacs-all-the-icons-fonts gost;
+  inherit (unstable) isync goimapnotify gnupg keychain pinentry emacs-all-the-icons-fonts gost;
   # more fonts
   inherit (self.vimPlugins) vim-devicons;
   # sweet theme dep
@@ -46,8 +46,6 @@ in {
   #inherit (nixgl.auto) nixGLDefault;
   inherit (nixgl) nixVulkanIntel;
   # wait for mesa-d3d12
-  # python
-  inherit (super.python39Packages) supervisor grip cffi ansible black flake8 ptpython;
   # pandas isort setuptools timeago cython
   # deps for thefuck
   # colorama decorator psutil pyte;
