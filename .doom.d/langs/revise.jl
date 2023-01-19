85f166126fee12d13e68a69848822292a2a613c7
untoreh <francesco.giannelli@yahoo.it>
2023-01-19 10:09:01 +0100

    fix(julia): call pkg precopmile directly
    for parallel comp. Ensure that it is done before `using` the module

--- .doom.d/langs/revise.jl
+++ .doom.d/langs/revise.jl
@@ -6,8 +6,10 @@ include("debug_packages.jl")
 let proj = Pkg.project()
     isnothing(proj.name) ||
         if "Revise" ∈ keys(proj.dependencies)
-            @async begin @eval using Revise
-                mod = Symbol(proj.name)
+            mod = Symbol(proj.name)
+            @eval using Revise
+            @async begin
+                Pkg.precompile()
                 @eval using $mod
                 @eval using Base.Meta
                 doeval() = eval(Meta.parse("Revise.revise($mod)"))
@@ -17,24 +19,29 @@ let proj = Pkg.project()
                     Pkg.precompile()
                     doeval()
                 end
+                flush(stdout)
+                display("done!")
+                flush(stdout)
             end
         else
+            @eval using TOML
+            projpath = dirname(proj.path)
+            testpath = joinpath(projpath, "test")
+            testcfg = joinpath(testpath, "Project.toml")
+            if !ispath(testcfg) || "Revise" ∉ keys(TOML.parsefile(testcfg)["deps"])
+                mkpath(testpath)
+                Pkg.activate(testpath)
+                Pkg.add("Revise")
+                Pkg.activate(projpath)
+            end
+            testpath ∉ LOAD_PATH && push!(LOAD_PATH, testpath)
+            @eval using Revise
             @async begin
-                @eval using TOML
-                projpath = dirname(proj.path)
-                testpath = joinpath(projpath, "test")
-                testcfg = joinpath(testpath, "Project.toml")
-                if !ispath(testcfg) || "Revise" ∉ keys(TOML.parsefile(testcfg)["deps"])
-                    mkpath(testpath)
-                    Pkg.activate(testpath)
-                    Pkg.add("Revise")
-                    Pkg.activate(projpath)
-                end
-                testpath ∉ LOAD_PATH && push!(LOAD_PATH, testpath)
-                @eval using Revise
+                Pkg.precompile()
                 Revise.revise()
                 @eval using $(Symbol(proj.name))
                 eval(Meta.parse("Revise.revise($(proj.name))"))
+                display("done!")
             end
         end
 end
