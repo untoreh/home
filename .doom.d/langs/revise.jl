9b0d4d890c6b4b5af6de130cd1d33d383a1b18db
untoreh <francesco.giannelli@yahoo.it>
2023-03-06 19:15:53 +0100

    langs(julia): use progressbar for startup

--- .doom.d/langs/revise.jl
+++ .doom.d/langs/revise.jl
@@ -4,30 +4,49 @@ using Pkg
 ENV["JULIA_PKG_PRECOMPILE_AUTO"] = false
 include("dev_packages.jl")
 using Suppressor
+using Term.Progress
 let proj = Pkg.project()
+    pbar = ProgressBar(columns=:minimal, transient=true)
+    prog = addjob!(pbar, description="Compiling...", N=9)
+    prog!() = begin
+        update!(prog)
+        render(pbar)
+    end
+    # OhMyREPL.input_prompt!(project_prompt("compiling..."))
+    start!(pbar)
     if !isnothing(proj.name)
         @sync begin
             @async @eval using Revise
+            prog!() # 2
             @async @eval using OhMyREPL
         end
+        prog!() # 3
         mod = Symbol(proj.name)
-        @async begin
-            OhMyREPL.input_prompt!(project_prompt("compiling..."))
+        global comp_task
+        comp_task = @async begin
             try
+                prog!() # 4
                 @suppress begin
                     Pkg.precompile()
                 end
+                prog!() # 5
                 @eval using $mod
+                prog!() # 6
                 @eval using Base.Meta
+                prog!() # 7
                 eval(Meta.parse("Revise.revise($mod)"))
-                OhMyREPL.input_prompt!(project_prompt())
+                prog!() # 8
             catch e
-                OhMyREPL.input_prompt!(project_prompt("error!"))
                 global last_error
                 last_error = () -> showerror(stdout, e)
+                OhMyREPL.input_prompt!(project_prompt("error!"))
+            finally
+                stop!(pbar)
             end
+            prog!() # 9
         end
     else
+        stop!(pbar)
         @warn "No project found, not loading Revise."
     end
 end;
