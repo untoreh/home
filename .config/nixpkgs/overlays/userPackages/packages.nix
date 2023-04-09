2af220b012f529a3c435dc78af1cff036b807c36
untoreh <francesco.giannelli@yahoo.it>
2023-04-09 18:05:58 +0200

    packages

--- .config/nixpkgs/overlays/userPackages/packages.nix
+++ .config/nixpkgs/overlays/userPackages/packages.nix
@@ -11,28 +11,26 @@ let
 in {
   inherit (unstable)
   # shell
-    browsh zsh git-hub git-lfs delta pet tmux fzf fd
+    zsh git-hub git-lfs delta pet tmux fzf fd
     ripgrep zoxide exa nushell starship direnv bat pastel
     # utils
-    wego translate-shell weechat krita calibre koreader youtube-dl djvulibre unoconv wordnet aspell ghostscript
+    wego translate-shell weechat calibre koreader youtube-dl djvulibre unoconv wordnet aspell ghostscript
+    # images
+    # krita
     # files and backups
     duplicacy borgbackup rclone syncthing inotify-tools
-    # desktop
-    mako scrot pywal
-    # reduntant
-    # xclip wl-clipboard
     # theme
-    noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji-blob-bin font-awesome
+    pywal noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-emoji-blob-bin font-awesome
     noto-fonts-extra sweet emojione fantasque-sans-mono julia-mono fira fira-code fira-code-symbols hack-font ia-writer-duospace
     powerline-fonts material-design-icons barlow
     # system (don't use fish from nix because of locales)
     man less thefuck glibcLocales tini libqalculate dhcp bandwhich trickle consul lld
-    haproxy rsync sshpass pandoc zip cachix msmtp catatonit
+    haproxy rsync sshpass pandoc zip cachix msmtp
 
     # langs
     pup nixfmt golangci-lint shfmt html-tidy nodejs shellcheck jq enchant2 ispell languagetool
     chrpath pipenv jre;
-  inherit (unstable) isync goimapnotify gnupg keychain pinentry emacs-all-the-icons-fonts;
+  inherit (unstable) mu isync goimapnotify gnupg keychain pinentry emacs-all-the-icons-fonts;
   # more fonts
   inherit (self.vimPlugins) vim-devicons;
   # sweet theme dep
@@ -43,10 +41,8 @@ in {
   inherit (self.weechatScripts) weechat-matrix;
   # gpu, waiting for nixpkgs integration
   # nixGLDefault, disabled because of "unsupported-package" issue
-  inherit (nixgl.auto) nixGLDefault;
+  # inherit (nixgl.auto) nixGLDefault;
   # inherit (nixgl) nixVulkanIntel;
-  # wait for mesa-d3d12
-  # pandas isort setuptools timeago cython
   # deps for thefuck
   # colorama decorator psutil pyte;
 }
