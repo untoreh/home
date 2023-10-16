f00cf7358e6202547766265771e91877670dd4e1
untoreh <francesco.giannelli@yahoo.it>
2023-10-16 13:48:30 +0200

    desktop: systemd cleanup

--- .config/systemd/scripts/kde-autostart.sh
+++ .config/systemd/scripts/kde-autostart.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 systemctl --user import-environment
 systemctl --user restart gpg-agent.service
 systemctl --user start p2p.target
-
-## update tmux DBUS vars
-~/.config/sway/scripts/tmux-update-env.sh
