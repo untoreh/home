98e45166332c32f263a734f6a8c9b7d18ede2498
untoreh <francesco.giannelli@yahoo.it>
2023-10-16 13:39:56 +0200

    emacs: fix fonts check

--- .doom.d/missing-fonts.el
+++ .doom.d/missing-fonts.el
@@ -1,6 +1,6 @@
 ;;; missing-fonts.el -*- lexical-binding: t; -*-
 
-(defvar required-fonts '("Hack.*" "iA Writer Duospace" "Barlow" "Fantasque Sans Mono" "JuliaMono"))
+(defvar required-fonts '("Hack.*" "iA Writer Duo S" "Barlow" "Fantasque Sans Mono" "JuliaMono"))
 
 (defvar available-fonts
   (delete-dups (or (font-family-list)
@@ -18,15 +18,15 @@
 
 (if missing-fonts
     (pp-to-string
-     `(unless noninteractive
-        (add-hook! 'doom-init-ui-hook
-          (run-at-time nil nil
-                       (lambda ()
-                         (message "%s missing the following fonts: %s"
-                                  (propertize "Warning!" 'face '(bold warning))
-                                  (mapconcat (lambda (font)
-                                               (propertize font 'face 'font-lock-variable-name-face))
-                                             ',missing-fonts
-                                             ", "))
-                         (sleep-for 0.5))))))
+     (unless noninteractive
+       (add-hook! 'doom-init-ui-hook
+         (run-at-time nil nil
+                      (lambda ()
+                        (message "%s missing the following fonts: %s"
+                                 (propertize "Warning!" 'face '(bold warning))
+                                 (mapconcat (lambda (font)
+                                              (propertize font 'face 'font-lock-variable-name-face))
+                                            missing-fonts
+                                            ", "))
+                        (sleep-for 0.5))))))
   ";; No missing fonts detected")
