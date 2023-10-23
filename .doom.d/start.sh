84f5b979e271f1c78ad5ab73198a9d8bdcddd352
untoreh <francesco.giannelli@yahoo.it>
2023-10-23 13:44:46 +0200

    emacs: change systemd target

--- .doom.d/start.sh
+++ .doom.d/start.sh
@@ -9,11 +9,16 @@ cd ~/.emacs.d
 # doesn't exist
 mkdir -p /tmp/__site
 
+systemdvar(){
+	systemctl --user import-environment WAYLAND_DISPLAY,XAUTHORITY,XDG_CURRENT,XDG_SEAT,XDG_SESSION_CLASS,XDG_SESSION_TYPE,XDG_VTNR
+}
+
 # export EMACS=$(which emacs)
 run_emacs() {
 	# $EMACS --fg-daemon=server
 	if [ -v TOOLBOX_PATH ]; then
 		if [ -v SYSTEMD_EXEC_PID ]; then
+			systemdvars
 			/usr/bin/toolbox run /usr/bin/emacs $@
 		else
 			/usr/bin/emacs $@
