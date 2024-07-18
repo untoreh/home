4141b2e5b720fa4a0235c1c7b373d110483f8c59
untoreh <francesco.giannelli@yahoo.it>
2024-07-18 15:58:02 +0200

    fish: use kitten ssh

--- .config/fish/functions/net.fish
+++ .config/fish/functions/net.fish
@@ -1,7 +1,7 @@
 ## Remote
 
 # ## network
-alias ssh "TERM=screen /usr/bin/ssh"
+alias ssh "kitten ssh"
 alias rtn "ssh -t root@192.168.1.1 SHELL=/bin/bash exec /bin/bash -li"
 alias rft "ssh root@192.168.1.1 COLUMNS=\$COLUMNS LINES=\$LINES TERM=linux iftop -i br-lan -m 20m"
 alias ift " _ iftop -i enp33s0 -m 20m"
@@ -16,20 +16,20 @@ alias chgho "ssh localhost -c none"
 
 function pgt
     set -q argv[1]; or set argv[1] "google.it"
-	  /bin/ping $argv[1] | grep --line-buffered 'time=' | sed -r 's/.*time=((.*)ms)/\1/'
+    /bin/ping $argv[1] | grep --line-buffered 'time=' | sed -r 's/.*time=((.*)ms)/\1/'
 end
 
 ## fetch the first hostname from ip (fragment)
 function host_from_ssh
     set query $argv[1]
-    sed -r "/\s*hostname.*($query)[0-9\.\s]*\$/Iq" < $HOME/.ssh/config | read -z first_host
+    sed -r "/\s*hostname.*($query)[0-9\.\s]*\$/Iq" <$HOME/.ssh/config | read -z first_host
     # echo $first_host
     echo $first_host | tac | read -z rev
     # echo $first_host
     echo $rev | sed -n -r '/^host /I{s/\s*host\s+//Ip;q;};' || echo "can only match ips :("
 end
 
-alias hossh "host_from_ssh"
+alias hossh host_from_ssh
 
 ## fetch hostname from hostname (fragment)
 function complete_hostname_from_ssh
@@ -44,8 +44,8 @@ alias chssh="complete_hostname_from_ssh"
 
 function ip_from_ssh
     set -lq part
-    [ "$argv[1]" = "-p" ] && set part Port && shift || set part HostName
-    sed -n -e '/'"$argv[1]"'/I,/'$part'/Ip' < ~/.ssh/config | read -z hostnames
+    [ "$argv[1]" = -p ] && set part Port && shift || set part HostName
+    sed -n -e '/'"$argv[1]"'/I,/'$part'/Ip' <~/.ssh/config | read -z hostnames
     echo $hostnames | sed '/^#.*$/d' | read -z hostnames
     echo $hostnames | sed -n 's/\s*'$part'\s*//Ip' | read -z ips
     set ip (echo $ips | grep '.' | sort -u | tail -1)
@@ -56,8 +56,8 @@ alias possh="ip_from_ssh -p"
 
 function ips_from_ssh
     set -q part
-    [ $argv[1] = "-p" ] && set part Port && shift || set part HostName
-    sed -n -e '/'"$1"'/I,/Host\s/{/^#.*$/d;s/.*'"$part"'\s*//Ip}' < ~/.ssh/config | read -z ips
+    [ $argv[1] = -p ] && set part Port && shift || set part HostName
+    sed -n -e '/'"$1"'/I,/Host\s/{/^#.*$/d;s/.*'"$part"'\s*//Ip}' <~/.ssh/config | read -z ips
     echo $ips | sort -u
 end
 alias ipsSsh="ips_from_ssh"
